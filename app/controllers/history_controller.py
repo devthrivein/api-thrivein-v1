b5b8b9d2c13066a036c1ec9a90799d97af958faf
Danar Rafiardi Ahmad <danarbaadilla12@gmail.com>
2023-12-09 14:10:32 +0800

    CPT-78# added icon_url in history response

--- app/controllers/history_controller.py
+++ app/controllers/history_controller.py
@@ -2,22 +2,32 @@ from flask import jsonify
 from app.config.config import db
 
 def history_order(user_id):
-    #query firestore untuk mendapatkan history order dari user
+    # Query firestore to get history order from the user
     history_ref = db.collection('order').where('user_id', '==', user_id)
     history_results = history_ref.stream()
 
-    #list untuk data 'history_order'
+    # List for data 'history_order'
     history_order = []
     for result in history_results:
         history_data = result.to_dict()
+
+        # Get service_id from the order
+        service_id = history_data.get('service_id')
+
+        # Query firestore to get service info based on service_id
+        service_ref = db.collection('services').document(service_id)
+        service_data = service_ref.get().to_dict()
+
+        # Build history_order_info with icon_url from services
         history_order_info = {
             'order_id': history_data.get('order_id'),
             'title': history_data.get('title'),
-            'transaction_date': history_data.get('transaction_date')
+            'transaction_date': history_data.get('transaction_date'),
+            'icon_url': service_data.get('icon_url')
         }
         history_order.append(history_order_info)
-    
-    #return response
+
+    # Return response
     response = {'history_order': history_order}
     return jsonify(response), 200
 
