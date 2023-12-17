6bc692e6f97d2b7095819272f54a834a4b2a7e97
Danar Rafiardi Ahmad <danarbaadilla12@gmail.com>
2023-12-17 09:14:08 +0800

    CPT-101 # added function to get icon_url

--- app/controllers/waiting_controller.py
+++ app/controllers/waiting_controller.py
@@ -13,10 +13,25 @@ def waiting_list(user_id):
     list_order = []
     for result in list_result:
         list_data = result.to_dict()
+        # Mendapatkan service_id dari order
+        service_id = list_data.get('service_id')
+
+        # Query firestore untuk mendapatkan info service berdasarkan service_id
+        service_ref = db.collection('services').document(service_id)
+        service_data = service_ref.get().to_dict()
+
+        # Memastikan service dengan service_id yang diberikan ada
+        if not service_data:
+            return jsonify({"error": "Service not found"}), 404
+
+         # Mendapatkan icon_url dari service_data
+        icon_url = service_data.get('icon_url')
+
         list_data_info = {
             'order_id': list_data.get('order_id'),
             'title': list_data.get('title'),
-            'transaction_date': list_data.get('transaction_date')
+            'transaction_date': list_data.get('transaction_date'),
+            'icon_url': icon_url
         }
         list_order.append(list_data_info)
 
@@ -30,9 +45,24 @@ def detail_waiting_order(order_id):
     detail_waiting_ref = db.collection('order').document(order_id)
     detail_result = detail_waiting_ref.get().to_dict()
 
+    # Mendapatkan service_id dari order
+    service_id = detail_result.get('service_id')
+
+    # Query firestore untuk mendapatkan info service berdasarkan service_id
+    service_ref = db.collection('services').document(service_id)
+    service_data = service_ref.get().to_dict()
+
+    # Memastikan service dengan service_id yang diberikan ada
+    if not service_data:
+        return jsonify({"error": "Service not found"}), 404
+
+    # Mendapatkan icon_url dari service_data
+    icon_url = service_data.get('icon_url')
+
     # Exclude key yang tidak diperlukan
-    excluded_fields = ['user_id', 'address', 'consultation_id']
+    excluded_fields = ['is_order_now']
 
     # return response
     response = {key: value for key, value in detail_result.items() if key not in excluded_fields}
+    response['icon_url'] = icon_url
     return jsonify(response), 200
